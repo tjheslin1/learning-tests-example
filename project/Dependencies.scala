ce8367d06f10398bad0a5b8a130a8f4cfced671d
Thomas Heslin <tjheslin1@gmail.com>
2018-05-07 18:47:32 +0100

    More Json4s tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,9 @@ import sbt._
 object Dependencies {
 
   val circeVersion = "0.9.3"
+  val json4sVersion = "3.5.3"
 
-  val json4s: ModuleID = "org.json4s" %% "json4s-native" % "3.5.3"
+  val json4s = "org.json4s" %% "json4s-native" % json4sVersion
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.5" % Test
 
   val circeCore = "io.circe" %% "circe-core" % circeVersion
--- src/test/scala/Json4sLearningSpec.scala
+++ src/test/scala/Json4sLearningSpec.scala
@@ -5,6 +5,7 @@ class Json4sLearningSpec extends WordSpec with Matchers {
 
   import org.json4s._
   import org.json4s.native.JsonMethods._
+  import org.json4s.native.Serialization._
 
   implicit val formats = DefaultFormats
 
@@ -17,6 +18,16 @@ class Json4sLearningSpec extends WordSpec with Matchers {
       parse(greyhoundJson).extract[Greyhound] shouldBe Greyhound("santasLittleHelper", 42)
       parse(westieJson).extract[Westie] shouldBe Westie("joey", true)
       parse(patterdaleJson).extract[Patterdale] shouldBe Patterdale("woody", true)
+    }
+
+    "produce JSON from domain model" in {
+      val greyhound = Greyhound("santasLittleHelper", 42)
+      val westie = Westie("joey", true)
+      val patterdale = Patterdale("woody", true)
+
+      write[Greyhound](greyhound) shouldBe """{"petName":"santasLittleHelper","runningSpeed":42}"""
+      write[Westie](westie) shouldBe """{"petName":"joey","goodBoy":true}"""
+      write[Patterdale](patterdale) shouldBe """{"petName":"woody","trained":true}"""
     }
   }
 }
